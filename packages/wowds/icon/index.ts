d9aadad8f35b1f929b945b4f3cda53fa8e0064b0
tablemin03 <vs201466@naver.com>
2026-08-04 14:20:27 +0900

    feat: index에 export 추가

--- packages/wowds/icon/index.ts
+++ packages/wowds/icon/index.ts
@@ -1,2 +1,46 @@
-// 아이콘. (추후 추가)
-export {}
+export { BlogIcon, type BlogIconProps, type BlogSize } from './src/assets/blog'
+
+export {
+  ChevronIcon,
+  type ChevronDirection,
+  type ChevronIconProps,
+  type ChevronSize,
+} from './src/assets/chevron'
+
+export { GithubIcon, type GithubIconProps, type GithubSize } from './src/assets/github'
+
+export {
+  InstagramIcon,
+  type IconProps as InstagramIconProps,
+  type IconSize as InstagramIconSize,
+} from './src/assets/instagram'
+
+export {
+  KakaotalkBarIcon,
+  type KakaotalkBarIconProps,
+  type KakaotalkBarSize,
+} from './src/assets/kakaotalkBar'
+
+export { KakaotalkIcon, type KakaotalkIconProps, type KakaotalkSize } from './src/assets/kakaotalk'
+
+export {
+  LinkIcon,
+  type IconProps as LinkIconProps,
+  type IconSize as LinkIconSize,
+} from './src/assets/link'
+
+export { PortfolioIcon, type PortfolioIconProps, type PortfolioSize } from './src/assets/portfolio'
+
+export { RadioIcon, type RadioIconProps, type RadioStatus } from './src/assets/radioButton'
+
+export {
+  StarIcon,
+  type IconProps as StarIconProps,
+  type IconSize as StarIconSize,
+} from './src/assets/star'
+
+export {
+  StatusIndicator,
+  type StatusIndicatorProps,
+  type StatusIndicatorSize,
+} from './src/assets/trackIndicator'
