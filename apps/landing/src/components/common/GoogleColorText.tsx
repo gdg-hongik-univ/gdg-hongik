d55dda51c6cb0bb92a8e76473bf6cc0dcc408d9d
tablemin03 <vs201466@naver.com>
2026-08-14 18:30:23 +0900

    feat: Google 로고 텍스트 컴포넌트 추가

---
+++ apps/landing/src/components/common/GoogleColorText.tsx
@@ -0,0 +1,20 @@
+const GOOGLE_COLORS = [
+  'text-core-blue-500',
+  'text-core-red-500',
+  'text-core-yellow-500',
+  'text-core-blue-500',
+  'text-core-green-500',
+  'text-core-red-500',
+] as const
+
+export function GoogleColorText() {
+  return (
+    <span aria-label="Google" className="font-medium inline-block">
+      {'Google'.split('').map((char, index) => (
+        <span key={index} className={GOOGLE_COLORS[index]} aria-hidden="true">
+          {char}
+        </span>
+      ))}
+    </span>
+  )
+}
