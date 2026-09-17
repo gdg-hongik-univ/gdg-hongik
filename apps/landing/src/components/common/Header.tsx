17d30ea6d4686b596353772675ef0d2b520b7aba
sangmin <sangmin@sangminui-MacBookPro.local>
2026-09-17 17:28:19 +0900

    feat: 로고 추가

--- apps/landing/src/components/common/Header.tsx
+++ apps/landing/src/components/common/Header.tsx
@@ -1,8 +1,10 @@
+import { LogoIcon } from '@gdg/wowds'
+
 const Header = () => {
   return (
     <header className="absolute inset-x-0 top-0 z-50 flex flex-row bg-white/30 backdrop-blur-[5px] px-4 py-5 s:p-7 xl:px-0 items-center justify-center">
-      <div className="mx-4 flex flex-1 xl:mx-0 xl:flex-none xl:w-[1280px] items-center justify-start gap-2.5">
-        <p className="flex h-4 items-center">나 이미지</p>
+      <div className="flex flex-1 xl:mx-0 xl:flex-none xl:w-[1280px] items-center justify-start gap-2.5">
+        <LogoIcon className="flex h-3.5 s:h-4" />
         <div className="flex gap-1 items-center">
           <h1 className="flex h-3.5 items-center font-title font-bold text-[18px] s:text-[20px] leading-none">
             GDG
