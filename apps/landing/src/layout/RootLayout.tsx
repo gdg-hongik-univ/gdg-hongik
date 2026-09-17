56dd0ca7be7703200818d659342fedd21d7ef7c6
sangmin <sangmin@sangminui-MacBookPro.local>
2026-09-17 17:54:28 +0900

    feat: 클릭 시 홈으로 오는 이벤트 추가

--- apps/landing/src/layout/RootLayout.tsx
+++ apps/landing/src/layout/RootLayout.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import Header from '../components/common/Header'
 
 export default function RootLayout() {
   return (
     <div className="relative min-h-screen flex flex-col w-full antialiased">
       <Header />
+      <ScrollRestoration />
 
       <main className="flex-1 w-full flex flex-col">
         <Outlet />
